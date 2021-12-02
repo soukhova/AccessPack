594f73ce7951aa68ac0d2ec81fece628e25b5596
Antonio Paez <paezha@gmail.com>
2021-12-02 11:13:10 -0500

    Worked on the initial version of the package with AS; sp_avail function still throwing some notes in CMD Check

--- R/sp_avail.R
+++ R/sp_avail.R
@@ -1,5 +1,5 @@
-# The input is an OD-table with Origins, Destinations, Population, Jobs, and a pre-calculated impedance function f
-#
+#' The input is an OD-table with Origins, Destinations, Population, Jobs, and a pre-calculated impedance function f
+#'
 #' @param x A data frame with origin-destination information, including identifiers for origins, destinations, population, opportunities, and a pre-calculated impedance function
 #' @param o_id  A character string with the name of the column in data frame x that contains the unique origin identifiers
 #' @param d_id  A character string with the name of the column in data frame x that contains the unique destination identifiers
@@ -8,50 +8,51 @@
 #' @param f     A character string with the name of the column in data frame x that contains the values of the impedance function
 #' @param alpha A number string with the name of the column in data frame x that contains the values of the impedance function
 #' @return A vector with the number of opportunities available to o_id from d_id
-#' @export
+#' @export sp_avail
 
-p_allocation <- function(x, o_id, d_id, pop, opp, f, alpha = 1){
-  x <- x %>%
-    rename(o = as.name(o_id),
-           d = as.name(d_id),
-           p = as.name(pop),
-           w = as.name(opp),
-           f = as.name(f))
+sp_avail <- function(x, o_id, d_id, pop, opp, f, alpha = 1){
+
+   x <- x %>%
+    dplyr::rename(#o = as.name(o_id),
+           d = as.name(d_id))#,
+           #p = as.name(pop),
+           #w = as.name(opp),
+           #f = as.name(f))
 
   sum_pop <- x %>%
-    distinct(o,
+    dplyr::distinct({{o_id}},
              .keep_all = TRUE) %>%
-    mutate(sum_pop = p^alpha) %>%
-    pull(sum_pop) %>%
+    dplyr::mutate(sum_pop = {{pop}}^alpha) %>%
+    dplyr::pull(sum_pop) %>%
     sum()
 
   x <- x %>%
-    mutate(f_p = p^alpha / sum_pop)
+    dplyr::mutate(f_p = {{pop}}^alpha / sum_pop)
 
   sum_impedance <- x %>%
-    group_by(d) %>%
-    summarize(sum_impedance = sum(f))
+    dplyr::group_by(d) %>%
+    dplyr::summarize(sum_impedance = sum({{f}}))
 
   x <- x %>%
-    left_join(sum_impedance,
+    dplyr::left_join(sum_impedance,
               by = "d")
 
   x <- x %>%
-    mutate(f_c = f / sum_impedance)
+    dplyr::mutate(f_c = {{f}} / sum_impedance)
 
   sum_pa <- x %>%
-    group_by(d) %>%
-    summarize(sum_pa= sum(f_p * f_c))
+    dplyr::group_by(d) %>%
+    dplyr::summarize(sum_pa= sum(f_p * f_c))
 
   x <- x %>%
-    left_join(sum_pa,
+    dplyr::left_join(sum_pa,
               by = "d")
 
   x <- x %>%
-    mutate(f_t = (f_p * f_c) / sum_pa)
+    dplyr::mutate(f_t = (f_p * f_c) / sum_pa)
 
   x <- x %>%
-    mutate(v_ij = w * f_t)
+    dplyr::mutate(v_ij = {{opp}} * f_t)
 
   return(x$v_ij)
 }
