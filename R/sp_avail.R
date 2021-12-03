1d5291b6dcfc9c33014b733535c2836751a8237d
Antonio Paez <paezha@gmail.com>
2021-12-02 23:13:19 -0500

    sp_aval3 is now sp_aval, moved the original version (with its warnings) to folder not-used-yet. There are still some warnings about no visible global bindings, not sure yet how to fix those.

--- R/sp_avail.R
+++ R/sp_avail.R
@@ -1,58 +1,58 @@
-#' The input is an OD-table with Origins, Destinations, Population, Jobs, and a pre-calculated impedance function f
+#' Third version of function with tidy evaluation. The input are the same:  an OD-table with Origins, Destinations, Population, Jobs, and a pre-calculated impedance function f
 #'
 #' @param x A data frame with origin-destination information, including identifiers for origins, destinations, population, opportunities, and a pre-calculated impedance function
-#' @param o_id  A character string with the name of the column in data frame x that contains the unique origin identifiers
-#' @param d_id  A character string with the name of the column in data frame x that contains the unique destination identifiers
-#' @param pop   A character string with the name of the column in data frame x that contains the population
-#' @param opp   A character string with the name of the column in data frame x that contains the opportunities
-#' @param f     A character string with the name of the column in data frame x that contains the values of the impedance function
+#' @param o_id  The name of the column in data frame x that contains the unique origin identifiers
+#' @param d_id  The name of the column in data frame x that contains the unique destination identifiers
+#' @param pop   The name of the column in data frame x that contains the population
+#' @param opp   The name of the column in data frame x that contains the opportunities
+#' @param f     The name of the column in data frame x that contains the values of the impedance function
 #' @param alpha A number string with the name of the column in data frame x that contains the values of the impedance function
 #' @return A vector with the number of opportunities available to o_id from d_id
+#' @importFrom rlang .data
 #' @export sp_avail
 
 sp_avail <- function(x, o_id, d_id, pop, opp, f, alpha = 1){
 
-   x <- x %>%
-    dplyr::rename(#o = as.name(o_id),
-           d = as.name(d_id))#,
-           #p = as.name(pop),
-           #w = as.name(opp),
-           #f = as.name(f))
+  o_id <- rlang::enquo(o_id)
+  d_id <- rlang::enquo(d_id)
+  pop <- rlang::enquo(pop)
+  opp <- rlang::enquo(opp)
+  f <- rlang::enquo(f)
 
   sum_pop <- x %>%
-    dplyr::distinct({{o_id}},
-             .keep_all = TRUE) %>%
-    dplyr::mutate(sum_pop = {{pop}}^alpha) %>%
+    dplyr::distinct(!!o_id,
+                    .keep_all = TRUE) %>%
+    dplyr::mutate(sum_pop = (!!pop)^alpha) %>%
     dplyr::pull(sum_pop) %>%
     sum()
 
   x <- x %>%
-    dplyr::mutate(f_p = {{pop}}^alpha / sum_pop)
+    dplyr::mutate(f_p = (!!pop)^alpha / sum_pop)
 
   sum_impedance <- x %>%
-    dplyr::group_by(d) %>%
-    dplyr::summarize(sum_impedance = sum({{f}}))
+    dplyr::group_by(!!d_id) %>%
+    dplyr::summarize(sum_impedance = sum(!!f))
 
   x <- x %>%
     dplyr::left_join(sum_impedance,
-              by = "d")
+                     by = rlang::as_name(d_id))
 
   x <- x %>%
-    dplyr::mutate(f_c = {{f}} / sum_impedance)
+    dplyr::mutate(f_c = !!f / sum_impedance)
 
   sum_pa <- x %>%
-    dplyr::group_by(d) %>%
+    dplyr::group_by(!!d_id) %>%
     dplyr::summarize(sum_pa= sum(f_p * f_c))
 
   x <- x %>%
     dplyr::left_join(sum_pa,
-              by = "d")
+                     by = rlang::quo_text(d_id))
 
   x <- x %>%
     dplyr::mutate(f_t = (f_p * f_c) / sum_pa)
 
   x <- x %>%
-    dplyr::mutate(v_ij = {{opp}} * f_t)
+    dplyr::mutate(v_ij = !!opp * f_t)
 
   return(x$v_ij)
 }
